ac6709184fa1aee61795451355413b5e4cfb4e03
Janet Warren <jewljbl13@aol.com>
2019-11-07 14:41:31 -0500

    add Course model and seed from json file, add pagination

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,11 +24,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 //Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -39,6 +44,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
